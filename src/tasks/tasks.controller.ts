102dbed1727b73b794c335d15eb1ad334abe5456
surendranj <Jishnu.Surendran@dowjones.com>
2021-07-27 21:07:17 +0530

    HTTP METHODS

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { title } from 'process';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 
@@ -24,6 +24,13 @@ export class TasksController {
     @Delete('/:id')
     deleteTask(@Param('id') id:string):void{
       return this.tasksService.deleteTask(id);
+    }
+    @Patch('/:id/status')
+    updateTaskStatus(
+      @Param('id') id:string,
+      @Body('status') status:TaskStatus
+    ):Task{
+      return this.tasksService.updateTaskStatus(id,status);
     }
 
 }
