4327405341f73c98fd31d979e3820d53cfe27ee1
surendranj <Jishnu.Surendran@dowjones.com>
2021-07-28 20:41:45 +0530

    pipes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid} from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-task-filter.dto';
 import { title } from 'process';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class TasksService {
@@ -13,7 +14,11 @@ export class TasksService {
         return this.tasks;
     }
     getTaskById(id:string):Task{
-        return this.tasks.find((task=>task.id === id));
+        const found = this.tasks.find((task=>task.id === id));
+        if(!found){
+            throw new NotFoundException(`Task with ID "${id}"`);
+        }
+        return found;
     }
     createTask(createTaskDto:CreateTaskDto):Task{
         const {title,description} = createTaskDto;
@@ -27,7 +32,8 @@ export class TasksService {
         return task;
     }
     deleteTask(id: string):void {
-     this.tasks = this.tasks.filter((task)=>task.id !== id);
+     const found = this.getTaskById(id);
+     this.tasks = this.tasks.filter((task)=>task.id !== found.id);
     }
     updateTaskStatus(id:string,status:TaskStatus){
         const task = this.getTaskById(id);
