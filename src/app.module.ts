c3ffd408094e82be1510f2b25409b23a09949949
surendranj <Jishnu.Surendran@dowjones.com>
2021-08-03 16:30:31 +0530

    deployement

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,30 +16,23 @@ import { configValidationSchema } from './config.schema';
       validationSchema:configValidationSchema
     }),
     TasksModule,
-    // TypeOrmModule.forRoot({
-    //   type:'postgres',
-    //   host:'localhost',
-    //   port:9004,
-    //   username:'postgres',
-    //   password:'postgres',
-    //   database:'task-management',
-    //   autoLoadEntities:true,
-    //   synchronize:true
-    // }),
     TypeOrmModule.forRootAsync({
       imports:[ConfigModule],
       inject:[ConfigService],
       useFactory:async(configService:ConfigService)=>{
+        const isProduction = configService.get('STAGE') == 'prod';
         return {
-          type:'postgres',
-          host:configService.get('DB_HOST'),
-          port:configService.get('DB_PORT'),
-          username:configService.get('DB_USERNAME'),
-          password:configService.get('DB_PASSWORD'),
-          database:configService.get('DB_DATABASE'),
-          autoLoadEntities:true,
-          synchronize:true
-        }
+          ssl: isProduction,
+          extra: { ssl: isProduction ? { rejectUnauthorized: false } : null },
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
       }
     }),
     AuthModule
